3391f4fc05a40838bb68c2cea266d99363f3dc27
hnean0124 <hnean0124@gmail.com>
2025-05-22 15:32:05 +0900

    Update udp_laptop_test7.py

--- udp_laptop_test7.py
+++ udp_laptop_test7.py
@@ -1,34 +1,27 @@
-#test_9와 연동
 import cv2
 import socket
 import struct
 import time
 
-# UDP 소켓 설정
-FRONT_CAMERA_PORT = 5005  # 전면 카메라 포트
-RIGHT_CAMERA_PORT = 5006  # 측면 카메라 포트
-TCP_IP = "223.194.129.240"  # 노트북 IP 주소
+FRONT_CAMERA_PORT = 5005
+RIGHT_CAMERA_PORT = 5006
+TCP_IP = "223.194.136.87"
 
-# 두 개의 소켓 생성
 front_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 right_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 front_socket.connect((TCP_IP, FRONT_CAMERA_PORT))
 right_socket.connect((TCP_IP, RIGHT_CAMERA_PORT))
 
-# 카메라 초기화
-front_camera = cv2.VideoCapture(0,cv2.CAP_DSHOW)  # 전면 카메라
-right_camera = cv2.VideoCapture(1,cv2.CAP_DSHOW)  # 측면 카메라
+front_camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+right_camera = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 
 if not front_camera.isOpened() or not right_camera.isOpened():
     print("⚠️ [ERROR] 카메라를 열 수 없습니다.")
     exit()
 
-#front_frame_counter = 0
-#right_frame_counter = 0
-
 def get_monotonic_timestamp():
-    return int(time.monotonic() * 1e6)
+    return int(time.monotonic() * 1e6)  # 마이크로초 단위
 
 def send_frame(socket, frame):
     _, buffer = cv2.imencode('.jpg', frame)
@@ -37,23 +30,21 @@ def send_frame(socket, frame):
     timestamp = get_monotonic_timestamp()
     header = struct.pack("QI", timestamp, size)
     socket.sendall(header + data)
-    
 
 while True:
-    # 전면 카메라 프레임 읽기
     ret_front, frame_front = front_camera.read()
     if ret_front:
-        front_frame_counter = send_frame(front_socket, frame_front)
-    
-    # 측면 카메라 프레임 읽기
-    ret_side, frame_side = right_camera.read()
-    if ret_side:
-        right_frame_counter = send_frame(right_socket, frame_side)
-    
+        send_frame(front_socket, frame_front)
+
+    ret_right, frame_right = right_camera.read()
+    if ret_right:
+        send_frame(right_socket, frame_right)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    time.sleep(0.05)  # 10ms 딜레이 추가
-    
+
+    time.sleep(0.05)
+
 front_camera.release()
 right_camera.release()
 front_socket.close()
